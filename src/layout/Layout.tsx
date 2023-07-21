d02c9b8a532373d7ecebd2f4f39f07b8a08e11a1
michaelslaton <michaelaaronphoto@gmail.com>
2023-07-21 04:14:25 -0700

    Continued experimentation

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,16 @@
-import "./layout.css";
+import { useLightTheme } from "../utils/contexts/themeContext";
+import LightSwitch from "./components/LightSwitch/LightSwitch";
 import Mockups from "./Mockups";
+import "./layout.css";
 
 const Layout: React.FC = () => {
+  const light = useLightTheme();
 
   return (
-    <>
+    <div className={`main-screen ${ light ? "" : "dark"}`}>
+      <LightSwitch/>
       <Mockups/>
-    </>
+    </div>
   );
 }
 
