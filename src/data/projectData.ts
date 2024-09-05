a8196ce83dfed7cdb53113917151c13d07abc06e
RenShadecrest <michaelaaronphoto@gmail.com>
2024-09-04 20:35:22 -0700

    began work on projects display

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -3,30 +3,20 @@ import weLoveMovies from '../assets/imgs/projectImages/weLoveMovies.jpg';
 import notflix from '../assets/imgs/projectImages/notflixr.jpg';
 import ProjectType from '../types/project.type';
 
-const projectList: ProjectType[] = [
+const projectData: ProjectType[] = [
   {
     id: 1,
-    show: true,
-    name: 'We Love Movies',
-    img: weLoveMovies,
-    type: '(Back-End)',
-    description: 'A back-end application intended to return data on movies, movie reviews, and theaters a movie is playing at.',
-    tech: ['Javascript', 'Node.js', 'Knex.js','Express.js'],
-    code: 'https://github.com/michaelslaton/we-love-movies-capstone',
+    show: false,
+    name: 'Remazon Prime',
+    img: '',
+    type: '(Full-Stack)',
+    description: '',
+    tech: ['Javascript','React.js', 'Firebase', 'HTML', 'Css', 'Node.js', 'Redux',],
+    code: 'https://github.com/GolamHossain9355/notflix-front',
+    codeb: 'https://github.com/GolamHossain9355/notflix-back',
   },
   {
     id: 2,
-    show: true,
-    name: 'Reservations Application',
-    img: resApp,
-    type: '(Full-Stack)',
-    description: 'A retro styled, full-stack web application that allows a restaurant to create, search, and edit reservations as well as manage seating inside the restaurant as their guests arrive.',
-    tech: ['Javascript','React.js','HTML','Css', 'Node.js', 'Knex.js','Express.js'],
-    code: 'https://github.com/michaelslaton/ms-restaurant-reservations-front',
-    codeb: 'https://github.com/michaelslaton/ms-restaurant-reservations-back',
-  },
-  {
-    id: 3,
     show: true,
     name: 'Notflix',
     img: notflix,
@@ -37,16 +27,26 @@ const projectList: ProjectType[] = [
     codeb: 'https://github.com/GolamHossain9355/notflix-back',
   },
   {
+    id: 3,
+    show: true,
+    name: 'We Love Movies',
+    img: weLoveMovies,
+    type: '(Back-End)',
+    description: 'A back-end application intended to return data on movies, movie reviews, and theaters a movie is playing at.',
+    tech: ['Javascript', 'Node.js', 'Knex.js','Express.js'],
+    code: 'https://github.com/michaelslaton/we-love-movies-capstone',
+  },
+  {
     id: 4,
-    show: false,
-    name: 'Remazon Prime',
-    img: '',
+    show: true,
+    name: 'Reservations Application',
+    img: resApp,
     type: '(Full-Stack)',
-    description: '',
-    tech: ['Javascript','React.js', 'Firebase', 'HTML', 'Css', 'Node.js', 'Redux',],
-    code: 'https://github.com/GolamHossain9355/notflix-front',
-    codeb: 'https://github.com/GolamHossain9355/notflix-back',
+    description: 'A retro styled, full-stack web application that allows a restaurant to create, search, and edit reservations as well as manage seating inside the restaurant as their guests arrive.',
+    tech: ['Javascript','React.js','HTML','Css', 'Node.js', 'Knex.js','Express.js'],
+    code: 'https://github.com/michaelslaton/ms-restaurant-reservations-front',
+    codeb: 'https://github.com/michaelslaton/ms-restaurant-reservations-back',
   },
 ];
 
-export default projectList;
+export default projectData;
