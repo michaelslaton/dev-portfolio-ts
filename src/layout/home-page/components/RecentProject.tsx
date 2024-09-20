cac96be475879354368371a924bf7a600ae9a170
RenShadecrest <michaelaaronphoto@gmail.com>
2024-09-19 20:22:44 -0700

    switched home page flex box to grid, added animations, adjusted sizing to fit an aspect ratio

--- src/layout/home-page/components/RecentProject.tsx
+++ src/layout/home-page/components/RecentProject.tsx
@@ -3,25 +3,39 @@ import { NavigateFunction, useNavigate } from 'react-router-dom';
 import projectData from '../../../data/projectData';
 import '../homePage.css';
 import ProjectType from '../../../types/project.type';
+import { useInView } from 'react-intersection-observer';
+import { useState } from 'react';
 
 const RecentProject: React.FC = () => {
   const navigate: NavigateFunction = useNavigate();
   const mostRecentProject: ProjectType | undefined = [...projectData].pop();
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref: visibilityRef, inView: visible } = useInView();
+  if(visible && isVisible !== true) setIsVisible(true);
 
   return (
-    <div className='recent-project__wrapper'>
-      <h2 className='widget-title'>Recent Project</h2>
+    <div
+      className='recent-project__wrapper'
+      ref={visibilityRef}
+    >
+      <h2
+        className={`widget-title ${isVisible ? 'slide-in' : 'slide-out'}`}
+      >
+        Recent Project
+      </h2>
 
-      <img
-        src={mostRecentProject?.img}
-        className='recent-project__img'
-        alt='Most Recent Project'
-      />
+      <div className={`${isVisible ? 'slide-up' : 'slide-down'}`}>
+        <img
+          src={mostRecentProject?.img}
+          className='recent-project__img'
+          alt='Most Recent Project'
+        />
+      </div>
 
-      <div className="see-more--wrapper">
+      <div className={`see-more--wrapper ${isVisible ? 'slide-in' : 'slide-out'}`}>
         <div
-          onClick={() => navigate("/projects")}
-          className="see-more"
+          onClick={() => navigate('/projects')}
+          className='see-more'
         >
           More Projects...
         </div>
--- src/layout/home-page/components/SkillsSummary.tsx
+++ src/layout/home-page/components/SkillsSummary.tsx
@@ -1,25 +1,34 @@
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import { default as skillsList } from "../../../data/skillsData";
-import Skill from "../../skills/components/Skill";
-import "../homePage.css";
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { default as skillsList } from '../../../data/skillsData';
+import Skill from '../../skills/components/Skill';
+import '../homePage.css';
+import { useInView } from 'react-intersection-observer';
+import { useState } from 'react';
 
 const SkillsSummary: React.FC = () => {
   const navigate: NavigateFunction = useNavigate();
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref: visibilityRef, inView: visible } = useInView();
+  if(visible && isVisible !== true) setIsVisible(true);
 
   return (
-    <div className="skills-summary__wrapper">
-      <h2 className='widget-title'>Skills Summary</h2>
+    <div className='skills-summary__wrapper' ref={visibilityRef}>
+      <h2
+        className={`widget-title ${isVisible ? 'slide-in' : 'slide-out'}`}
+      >
+        Skills Summary
+      </h2>
 
-      <div className="skills-summary__list">
+      <div className={`skills-summary__list ${isVisible ? 'slide-up' : 'slide-down'}`}>
         {skillsList.map((skill) => {
           if (skill.homePage) return <Skill key={skill.id} data={skill} />;
         })}
       </div>
       
-      <div className="see-more--wrapper">
+      <div className={`see-more--wrapper ${isVisible ? 'slide-in' : 'slide-out'}`}>
         <div
-          onClick={() => navigate("/skills")}
-          className="see-more"
+          onClick={() => navigate('/skills')}
+          className='see-more'
         >
           See more...
         </div>
