0465ca4abfbbdea7b5681c6ccecb3a4c1b3a8723
RenShadecrest <michaelaaronphoto@gmail.com>
2024-10-19 18:31:56 -0700

    created experience data set

---
+++ src/data/experienceData.ts
@@ -0,0 +1,17 @@
+import remazon from '../assets/imgs/projectImages/remazon.jpg';
+
+const experienceData = [
+  {
+    id: 1,
+    show: false,
+    name: 'Remazon Prime',
+    img: remazon,
+    type: '( Full-Stack )',
+    description: 'A web application for managing a Discord community, allowing users to create and display events on a calendar, track points for friendly competition, and enabling community leaders to communicate directly via messages of the day and notifications.',
+    tech: [19, 9, 14, 20, 17, 8, 3, 21, 4, 10, 13,],
+    screenShots: [],
+    code: 'https://github.com/michaelslaton/remazon',
+  },
+];
+
+export default experienceData;
