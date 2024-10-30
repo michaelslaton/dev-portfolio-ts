c4454de0d93bb22206c26e0ddf61f0c4a93ac027
RenShadecrest <michaelaaronphoto@gmail.com>
2024-10-29 21:57:38 -0700

    created experience list menu

--- src/layout/experience/ExperienceDisplay.tsx
+++ src/layout/experience/ExperienceDisplay.tsx
@@ -6,7 +6,16 @@ import Experience from './components/Experience';
 import { useInView } from 'react-intersection-observer';
 import './experience.css';
 
+type ExperienceStateType = {
+  selectedExperience: ExperienceType | null;
+  showItems: boolean;
+};
+
 const ExperienceDisplay: React.FC = () => {
+  const [ experienceState, setExperienceState ] = useState<ExperienceStateType>({
+    selectedExperience: null,
+    showItems: true,
+  });
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const { ref: visibilityRef, inView: visible } = useInView();
   if (visible && isVisible !== true) setIsVisible(true);
@@ -21,13 +30,32 @@ const ExperienceDisplay: React.FC = () => {
       </div>
       <Divider />
 
-      <div
-        className='experience-list__wrapper'
-        ref={visibilityRef}
-      >
-        { formattedExperienceList.map((experience)=>(
-          <Experience data={experience}/>
-        )) }
+      <div className='experience-display__container'>
+        <div
+          className='experience-list__wrapper'
+          ref={visibilityRef}
+        >
+          { formattedExperienceList.map((experience,i)=>(
+            <div
+              key={experience.id}
+              onClick={() => setExperienceState({
+                showItems: !experienceState.showItems,
+                selectedExperience: experience,
+              })}
+              style={{transitionDelay: `${i * 50}ms`}}
+              className={`projects__list-item 
+                ${experience.id === experienceState.selectedExperience?.id ? 'selected' : ''}
+                ${isVisible ? 'projects__list-item__slide-up' : 'projects__list-item__slide-down'}
+                ${experienceState.showItems || experienceState.selectedExperience?.id === experience.id ? 'show' : 'no-show'}
+                `}
+            >
+              {experience.name}
+            </div>
+          )) }
+        </div>
+
+        {experienceState.selectedExperience && <Experience data={experienceState.selectedExperience} />}
+
       </div>
     </div>
   );
