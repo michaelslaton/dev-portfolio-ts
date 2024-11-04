33889a367064a252b4e09a4d8cfd7ff0f7faf3b4
RenShadecrest <michaelaaronphoto@gmail.com>
2024-11-03 18:53:09 -0800

    began construction of experience component

--- src/layout/experience/components/Experience.tsx
+++ src/layout/experience/components/Experience.tsx
@@ -9,7 +9,25 @@ const Experience: React.FC<ExperienceProps> = ({ data }) => {
 
   return (
     <div className='experience__wrapper'>
-      {data.name}
+      <div className='experience__main-block'>
+        <img className='experience__img' src={data.img} alt={data.name}/>
+        <div className='experience__header'>
+          <div className='experience__title'>
+            <h2>{data.name}</h2>
+            <h3>{data.role}</h3>
+          </div>
+          <div className='experience__description'>
+            {data.description}
+          </div>
+          <div className='experience__bullet-points-wrapper'>
+            {data.bulletPoints?.map((bullet)=>(
+              <div className='experience__bullet-point'>
+                - {bullet}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
