376fc6a349a33ced71bf86c241035563d3a14c8f
RenShadecrest <michaelaaronphoto@gmail.com>
2024-11-04 18:25:02 -0800

    continued development of experience component

--- src/layout/experience/components/Experience.tsx
+++ src/layout/experience/components/Experience.tsx
@@ -1,4 +1,8 @@
+import months from "../../../data/months";
 import ExperienceType from "../../../types/experienceType";
+import { faAngleRight } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import '../experience.css';
 
 type ExperienceProps = {
@@ -12,20 +16,32 @@ const Experience: React.FC<ExperienceProps> = ({ data }) => {
       <div className='experience__main-block'>
         <img className='experience__img' src={data.img} alt={data.name}/>
         <div className='experience__header'>
+
           <div className='experience__title'>
             <h2>{data.name}</h2>
             <h3>{data.role}</h3>
           </div>
+
+          <div className='experience__dates'>
+            { data.endDate ?
+              <>{`${months[data.startDate.getMonth()]} ${data.startDate.getFullYear()}`} - {`${months[data.endDate.getMonth()]} ${data.endDate.getFullYear()}`}</>
+            :
+              <>{`${months[data.startDate.getMonth()]} ${data.startDate.getFullYear()}`} - Current</>
+            }
+          </div>
+
           <div className='experience__description'>
             {data.description}
           </div>
+
           <div className='experience__bullet-points-wrapper'>
             {data.bulletPoints?.map((bullet)=>(
               <div className='experience__bullet-point'>
-                - {bullet}
+                <span className='experience__bullet-point--arrow'><FontAwesomeIcon icon={faAngleRight as IconProp}/></span> {bullet}
               </div>
             ))}
           </div>
+
         </div>
       </div>
     </div>
