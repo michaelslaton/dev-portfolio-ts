cbcf95321f7b348e6388259cda5e3f37ed63a00b
RenShadecrest <michaelaaronphoto@gmail.com>
2024-11-14 15:47:32 -0800

    redid skill component to make code more efficient, started reconstructing the skills display for the different skill list types

--- src/layout/skills/SkillsDisplay.tsx
+++ src/layout/skills/SkillsDisplay.tsx
@@ -1,19 +1,14 @@
-import {default as skillsList} from '../../data/skillsData';
 import Divider from '../components/divider/Divider';
-import Skill from './components/Skill';
-import { useInView } from 'react-intersection-observer';
 import { useState } from 'react';
-import { faList } from '@fortawesome/fontawesome-free-solid';
+import { faList, faTable } from '@fortawesome/fontawesome-free-solid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import './skills.css';
+import ListView from './components/ListView';
+import CategoryView from './components/CateoryView';
 
 const SkillsDisplay: React.FC = () => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
   const [ menuSelected, setMenuSelected ] = useState<number>(1);
-  const { ref: visibilityRef, inView: visible } = useInView();
-  const sortedSkillsList = skillsList.sort((a,b)=>{ return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;});
-  if(visible && isVisible !== true) setIsVisible(true);
   
   return (
     <div className='content__screen'>
@@ -36,7 +31,7 @@ const SkillsDisplay: React.FC = () => {
             className='skills__menu--item'
             onClick={()=> setMenuSelected(2)}
           >
-            <FontAwesomeIcon icon={faList as IconProp}/>
+            <FontAwesomeIcon icon={faTable as IconProp}/>
           </span>
           <span
             className='skills__menu--item'
@@ -47,88 +42,17 @@ const SkillsDisplay: React.FC = () => {
           <div className={`skills__menu--highlight ${menuSelected === 1 && 'left'} ${menuSelected === 2 && 'center'} ${menuSelected === 3 && 'right'}`}/>
         </div>
       </div>
-
-      <div
-        ref={visibilityRef}
-        className='skills__display'
-      >
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Language
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('language')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
-        
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Front-End
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('front-end')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
-
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Back-End
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('back-end')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
-
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Version Control
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('version-control')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
-
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Testing
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('testing')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
-
-        <section>
-          <h3 className={`skills-list__title ${isVisible ? 'slide-in' : 'slide-out'}`}>
-            Other
-          </h3>
-          <div className={`skills-list__wrapper slide-down ${isVisible ? 'slide-up' : ''}`}>
-            { sortedSkillsList.map((skill)=> {
-              if(skill.category.includes('other')) return (
-                <Skill key={skill.id} data={skill}/>
-              );
-            })}
-          </div>
-        </section>
+                
+      <div className="skills__display">
+        { menuSelected === 1 &&
+          <ListView/>
+        }
+        { menuSelected === 2 &&
+          <CategoryView/>
+        }
+        { menuSelected === 3 &&
+          <ListView/>
+        }
       </div>
 
     </div>
