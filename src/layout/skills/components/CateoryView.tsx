e50f96480510b759ce505cf9d54ab4f745d861f7
RenShadecrest <michaelaaronphoto@gmail.com>
2024-11-15 14:40:30 -0800

    continued development on category list, adjusted css for list view to be more generic for all view types

--- src/layout/skills/components/CateoryView.tsx
+++ src/layout/skills/components/CateoryView.tsx
@@ -1,11 +1,39 @@
+import {default as skillsList} from '../../../data/skillsData';
+import SkillType from '../../../types/skill.type';
+import Skill from './Skill';
 import '../skills.css';
 
 const CategoryView: React.FC = () => {
+  
+  const createCategoryListing = (title: string, arr: SkillType[]): JSX.Element => {
+    const includedSkills: SkillType[] = arr.filter((skill)=> skill.category.split(', ').includes(title));
+
+    return (
+      <div className='skill-display__category'>
+
+        <span>{title}</span>
+
+        <div className='skill-display__divider'/>
+
+        <div className='skill-display__skills-list'>
+          {includedSkills.map((skill)=>(
+            <Skill data={skill}/>
+          ))}
+        </div>
+        
+      </div>
+    );
+  };
 
   return (
-    <>
-      Categories
-    </>
+    <div className='skill-display__wrapper'>
+      { createCategoryListing('Front-End', skillsList) }
+      { createCategoryListing('Back-End', skillsList) }
+      { createCategoryListing('Language', skillsList) }
+      { createCategoryListing('Testing', skillsList) }
+      { createCategoryListing('Version-Control', skillsList) }
+      { createCategoryListing('Other', skillsList) }
+    </div>
   );
 };
 
