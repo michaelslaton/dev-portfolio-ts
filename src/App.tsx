d9095fbfc9912ab63fecacc88716e5266773c449
RenShadecrest <michaelaaronphoto@gmail.com>
2024-12-18 18:28:34 -0800

    code revisions, code simplification, removed experience and project displays and added list display, added font pre-loading, converted fonts to woff2

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Layout from './layout/Layout';
 import Error404 from './utils/errors/error404/Error404';
 import RouteError from './utils/errors/route-error/RouteError';
 import { createContext, useState } from 'react';
-import Projects from './layout/projects/ProjectsDisplay';
 import SkillsDisplay from './layout/skills/SkillsDisplay';
 import Contact from './layout/contact/Contact';
 import HomePage from './layout/home-page/HomePage';
-import ExperienceDisplay from './layout/experience/ExperienceDisplay';
 import PhotoLayout from './photo-layout/PhotoLayout';
+import projectData from './data/projectData';
+import ListDisplay from './layout/list-display/ListDisplay';
+import experienceData from './data/experienceData';
 
 export type ThemeType = 'light' | 'dark';
 export type ScreenType = 'main' | 'tools';
@@ -37,12 +38,12 @@ const routes: RouteObject[] = [
       },
       { 
         path: '/projects',
-        element: <Projects />,
+        element: <ListDisplay title='Projects' listData={projectData}/>,
         errorElement: <RouteError />
       },
       { 
         path: '/experience',
-        element: <ExperienceDisplay />,
+        element: <ListDisplay title='Experience' listData={experienceData}/>,
         errorElement: <RouteError />
       },
       { 
--- src/layout/list-display/components/ListItem.tsx
+++ src/layout/list-display/components/ListItem.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleDown } from '@fortawesome/fontawesome-free-solid';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import ProjectType from '../../../types/project.type';
 
-type ProjectListItemProps = { 
-  project: ProjectType; 
+type ListItemProps = { 
+  title: string; 
   isSelected: boolean; 
   isVisible: boolean; 
   showItems: boolean;
   onClick: () => void;
 };
 
-const ProjectListItem = React.memo(({ project, isSelected, isVisible, showItems, onClick }: ProjectListItemProps) => {
+const ListItem = React.memo(({ title, isSelected, isVisible, showItems, onClick }: ListItemProps) => {
   const getChevronIcon = () => {
     if (!isSelected) return <FontAwesomeIcon icon={faAngleLeft as IconProp} />;
     if (isVisible) return <FontAwesomeIcon icon={faAngleDown as IconProp} />;
@@ -22,16 +21,16 @@ const ProjectListItem = React.memo(({ project, isSelected, isVisible, showItems,
   return (
     <div
       onClick={onClick}
-      className={`projects__list-item 
+      className={`list-item 
         ${isSelected ? 'selected' : ''} 
-        ${isVisible ? 'projects__list-item__slide-up' : 'projects__list-item__slide-down'}
+        ${isVisible ? 'list-item__slide-up' : 'list-item__slide-down'}
         ${ showItems || isSelected ? 'show' : 'no-show'}
       `}
     >
       <span className='list-chevron'>{getChevronIcon()}</span>
-      {` ${project.name}`}
+      {` ${title}`}
     </div>
   );
 });
 
-export default ProjectListItem;
+export default ListItem;
