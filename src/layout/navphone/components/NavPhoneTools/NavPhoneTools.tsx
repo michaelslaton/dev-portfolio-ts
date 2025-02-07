7b087d79e2a21092c4d8f2e134799a20d445e17e
RenShadecrest <michaelaaronphoto@gmail.com>
2025-02-07 11:44:04 -0800

    deployment fix

--- src/layout/navphone/components/NavPhoneTools/NavPhoneTools.tsx
+++ src/layout/navphone/components/NavPhoneTools/NavPhoneTools.tsx
@@ -9,7 +9,7 @@ import {
  import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PortfolioState } from '../../../../App';
-import './NavPhoneTools.css';
+import './navPhoneTools.css';
 
 const NavPhoneTools = () => {
   const context = useContext(PortfolioState);
